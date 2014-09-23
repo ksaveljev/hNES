8dafc6bbc77478fa7278b7efc3493b0f07a07566
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2014-09-23 23:33:56 +0300

    A bit of reformatting

--- NES/Instruction.hs
+++ NES/Instruction.hs
@@ -1,5 +1,9 @@
 module NES.Instruction where
 
+import Data.Word (Word8)
+
+type Operand = Word8
+
 data AddressingMode = Implicit
                     | Accumulator
                     | Immediate
@@ -14,59 +18,17 @@ data AddressingMode = Implicit
                     | IndexedIndirect
                     | IndirectIndexed
 
-data Instruction = ADC
-                 | AND
-                 | ASL
-                 | BCC
-                 | BCS
-                 | BEQ
-                 | BIT
-                 | BMI
-                 | BNE
-                 | BPL
-                 | BRK
-                 | BVC
-                 | BVS
-                 | CLC
-                 | CLD
-                 | CLI
-                 | CLV
-                 | CMP
-                 | CPX
-                 | CPY
-                 | DEC
-                 | DEX
-                 | DEY
-                 | EOR
-                 | INC
-                 | INX
-                 | INY
-                 | JMP
-                 | JSR
-                 | LDA
-                 | LDX
-                 | LDY
-                 | LSR
-                 | NOP
-                 | ORA
-                 | PHA
-                 | PHP
-                 | PLA
-                 | PLP
-                 | ROL
-                 | ROR
-                 | RTI
-                 | RTS
-                 | SBC
-                 | SEC
-                 | SED
-                 | SEI
-                 | STA
-                 | STX
-                 | STY
-                 | TAX
-                 | TAY
-                 | TSX
-                 | TXA
-                 | TXS
-                 | TYA
+data Mnemonic = ADC | AND | ASL | BCC
+              | BCS | BEQ | BIT | BMI
+              | BNE | BPL | BRK | BVC
+              | BVS | CLC | CLD | CLI
+              | CLV | CMP | CPX | CPY
+              | DEC | DEX | DEY | EOR
+              | INC | INX | INY | JMP
+              | JSR | LDA | LDX | LDY
+              | LSR | NOP | ORA | PHA
+              | PHP | PLA | PLP | ROL
+              | ROR | RTI | RTS | SBC
+              | SEC | SED | SEI | STA
+              | STX | STY | TAX | TAY
+              | TSX | TXA | TXS | TYA
